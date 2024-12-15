f25f15050a60bb508ec0af116750951f0caad9c1
joyce <lindy6735@hotmail.com>
2024-12-15 14:14:54 +0800

    feat: 實做google登入

--- app/src/main/java/com/joyce/pickdays/MainActivity.kt
+++ app/src/main/java/com/joyce/pickdays/MainActivity.kt
@@ -2,10 +2,13 @@ package com.joyce.pickdays
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.joyce.pickdays.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        LoginActivity.startActivity(this)
     }
 }
--- app/src/main/java/com/joyce/pickdays/login/LoginActivity.kt
+++ app/src/main/java/com/joyce/pickdays/login/LoginActivity.kt
@@ -1,15 +1,27 @@
 package com.joyce.pickdays.login
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.joyce.pickdays.R
 import com.joyce.pickdays.databinding.ActivityLoginBinding
+import com.joyce.pickdays.util.mLog
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private val viewModel: LoginViewModel by viewModels()
+
+    companion object {
+        private const val RC_SIGN_IN = 100
+
+        fun startActivity(context: Context){
+            val intent = Intent(context, LoginActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +29,16 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.googleLogin.setOnClickListener {
+            mLog.i("click google login")
+            val signInIntent = viewModel.googleSignInIntent()
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+    }
 
+    override fun onActivityResult(requestCoded: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCoded, resultCode, data)
+        if (requestCoded == RC_SIGN_IN){
+            viewModel.handleLoginResult(data)
         }
     }
 }
